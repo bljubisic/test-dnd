27783266ab73c847a32132200a472886bae00253
Bratislav Ljubisic <bratislav.ljubisic@icloud.com>
2023-12-03 21:02:14 +0100

    Fixed some styling issues

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -73,117 +73,99 @@
         .replace("T", " ")
     };
   </script>
-  
   <p>Drag a framework/library from random to respected drop</p>
-  <div in:receive={{ key: 0 }} out:send={{ key: 0 }}>
-    <b>{Stack[0].name}</b>
-    <p
-      class:hovering={stackHover === Stack[0].name}
-      on:dragenter={() => (stackHover = Stack[0].name)}
-      on:dragleave={() => (stackHover = null)}
-      on:drop={(event) => drop(event, 0)}
-      ondragover="return false"
-    >
-      {#each Stack[0].items as item, itemIndex (item)}
-        <div
-          class="item"
-          in:receive={{ key: itemIndex }}
-          out:send={{ key: itemIndex }}
-          animate:flip={{ duration: 500 }}
-        >
-          <li draggable={true} on:dragstart={(event) => dragStart(event, 0, itemIndex)}>
-            {item.name}
-          </li>
-        </div>
-      {/each}
-    </p>
-  </div>
-  <div in:receive={{ key: 1 }} out:send={{ key: 1 }}>
-    <b>{Stack[1].name}</b>
-    <p
-      class:hovering={stackHover === Stack[1].name}
-      on:dragenter={() => (stackHover = Stack[1].name)}
-      on:dragleave={() => (stackHover = null)}
-      on:drop={(event) => drop(event, 1)}
-      ondragover="return false"
-    >
-      {#each Stack[1].items as item, itemIndex (item)}
-        <div
-          class="item"
-          in:receive={{ key: itemIndex }}
-          out:send={{ key: itemIndex }}
-          animate:flip={{ duration: 500 }}
-        >
-          <li draggable={true} on:dragstart={(event) => dragStart(event, 1, itemIndex)}>
-            {item.name}
-          </li>
-        </div>
-      {/each}
-    </p>
-  </div>
-  <div>
-    {#each Stack[1].items as item, itemIndex (item)}
-      <div
-        class="item"
-      >
-        <li>
-          <span style="white-space: pre-line">{item.question}</span>
-        </li>
-      </div>
-    {/each}
-  </div>
-  <div>
-    {#each Stack[1].items as item, itemIndex (item)}
-      <div
-        class="item"
-      >
-        <li>
-          {#await page = md(item.question)}
-            <p>...waiting</p>
-          {:then page}
-            {#each page as node}
-              <SurveyNode {node} bind:context {next} />
-            {/each}
-          {:catch error}
-            <p style="color: red">{error.message}</p>
-          {/await} 
-        </li>
-      </div>
-    {/each}
-  </div>
-  
-  <!-- {#each Stack as stack, stackIndex (stack)}
-    <div animate:flip in:receive={{ key: stackIndex }} out:send={{ key: stackIndex }}>
-      <b>{stack.name}</b>
-      <p
-        class:hovering={stackHover === stack.name}
-        on:dragenter={() => (stackHover = stack.name)}
+  <div class="container">
+    <div in:receive={{ key: 0 }} out:send={{ key: 0 }}>
+      <b>{Stack[0].name}</b>
+      <!-- svelte-ignore a11y-no-static-element-interactions -->
+      <div class="questions"
+        class:hovering={stackHover === Stack[0].name}
+        on:dragenter={() => (stackHover = Stack[0].name)}
         on:dragleave={() => (stackHover = null)}
-        on:drop={(event) => drop(event, stackIndex)}
+        on:drop={(event) => drop(event, 0)}
         ondragover="return false"
       >
-        {#each stack.items as item, itemIndex (item)}
+        {#each Stack[0].items as item, itemIndex (item)}
           <div
             class="item"
             in:receive={{ key: itemIndex }}
             out:send={{ key: itemIndex }}
             animate:flip={{ duration: 500 }}
           >
-            <li draggable={true} on:dragstart={(event) => dragStart(event, stackIndex, itemIndex)}>
-              {item}
+            <li draggable={true} on:dragstart={(event) => dragStart(event, 0, itemIndex)}>
+              {item.name}
             </li>
           </div>
         {/each}
-      </p>
+        </div>
     </div>
-  {/each} -->
+    <div class="work">
+      <b>{Stack[1].name}</b>
+      <div in:receive={{ key: 1 }} out:send={{ key: 1 }} class="target-design">
+        <!-- svelte-ignore a11y-no-static-element-interactions -->
+        <div class="target"
+          class:hovering={stackHover === Stack[1].name}
+          on:dragenter={() => (stackHover = Stack[1].name)}
+          on:dragleave={() => (stackHover = null)}
+          on:drop={(event) => drop(event, 1)}
+          ondragover="return false"
+        >
+          {#each Stack[1].items as item, itemIndex (item)}
+            <div
+              class="item"
+              in:receive={{ key: itemIndex }}
+              out:send={{ key: itemIndex }}
+              animate:flip={{ duration: 500 }}
+            >
+              <li draggable={true} on:dragstart={(event) => dragStart(event, 1, itemIndex)}>
+                {item.name}
+              </li>
+            </div>
+          {/each}
+        </div>
+      </div>
+      <div class="container-results">
+        {#each Stack[1].items as item, itemIndex (item)}
+        
+          <div class="results">
+            <div class="markdown">
+              <div
+                class="item"
+              >
+                <li>
+                  <span style="white-space: pre-line">{item.question}</span>
+                </li>
+              </div>
+            </div>
+            <div class="render">
+              <div class="item">
+                <li>
+                  {#await page = md(item.question)}
+                    <p>...waiting</p>
+                  {:then page}
+                    {#each page as node}
+                      <SurveyNode {node} bind:context {next} />
+                    {/each}
+                  {:catch error}
+                    <p style="color: red">{error.message}</p>
+                  {/await} 
+                </li>
+              </div>
+            </div>
+          </div>
+        {/each}
+      </div> 
+    </div>
+  </div>
+
   
   <style>
     .hovering {
       border-color: #454B1B;
     }
     .item {
-      display: inline; /* required for flip to work */
+      display: inline;
+      margin-top: 10px; /* required for flip to work */
     }
     li {
       background-color: #00FFFF;
@@ -198,9 +180,59 @@
     }
     p {
       border: solid green 1px;
+
+    }
+    .work {
       display: flex; /* required for drag & drop to work when .item display is inline */
-      height: 40px; /* needed when empty */
+      flex-direction: column;
+      width: 100%; /* needed when empty */
+      height: auto;
       padding: 10px;
+      border: solid green 1px;
+    }
+    .questions {
+      display: flex; /* required for drag & drop to work when .item display is inline */
+      flex-direction: column;
+      flex-grow: 1;
+      align-items: stretch;
+      width: 80px; /* needed when empty */
+      height: auto;
+      padding: 10px;
+      border: solid green 1px;
+    }
+    .target-design {
+      border: solid green 1px;
+    }
+    .target {
+      display: flex; /* required for drag & drop to work when .item display is inline */
+      flex-direction: column;
+      width: 95%; /* needed when empty */
+      height: 100%;
+      padding: 10px;
+      margin-left: auto;
+      margin-right: auto;
+    }
+    .container {
+      display: flex;
+      flex-direction: row;
+    }
+    .results {
+      display: flex;
+      flex-direction: column;
+      margin-top: 20px;
+    }
+    .markdown {
+      display: flex;
+      flex-direction: column;
+    }
+    .render {
+      display: flex;
+      flex-direction: column;
+    }
+    .container-results {
+      display: flex;
+      flex-direction: row;
+      margin-top: 20px;
     }
   </style>
   
